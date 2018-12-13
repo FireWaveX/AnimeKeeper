07a087bb52ff3b5c6d053d044e107fa8c1082423
FireWaveX <alexledeme@hotmail.fr>
2018-12-13 23:58:34 +0100

    the war is over

--- app/src/main/java/org/ledeme/animekeeper/ListOfAnime.java
+++ app/src/main/java/org/ledeme/animekeeper/ListOfAnime.java
@@ -3,10 +3,13 @@ package org.ledeme.animekeeper;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -17,14 +20,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.ledeme.animekeeper.databinding.ContentActivityLoginBinding;
+import org.ledeme.animekeeper.databinding.ContentAnimeListBinding;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.widget.GridLayout.VERTICAL;
 
@@ -33,19 +49,32 @@ public class ListOfAnime extends AppCompatActivity {
     public RecyclerView rv_animeList;
     public String name_title;
     public String genres_title;
+    public JSONArray data;
     Button btnAddAnime;
+    ListOfAnimeMV.AnimeListAdapter mAdapter;
+
+    private static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anime_list);
+        //setContentView(R.layout.activity_anime_list);
+
+        ListOfAnimeMV listOfAnimeMV = new ListOfAnimeMV();
+        ((ContentAnimeListBinding) DataBindingUtil.setContentView(this, R.layout.content_anime_list))
+                .setLoginVM(listOfAnimeMV);
+
+        mContext = this;
 
         IntentFilter intentFilter = new IntentFilter(ANIME_LIST_UPDATE);
         LocalBroadcastManager.getInstance(this).registerReceiver(new AnimeUpdate(),intentFilter);
 
         //Set other intents/activity
         final Intent addAnime = new Intent(this, AddAnime.class);
+        final Intent listAnime = new Intent(this, ListOfAnime.class);
+        ///final Intent animeDetail = new Intent(this, AnimeDetail.class);
 
         name_title = getResources().getString(R.string.rv_anime_list_name);
         rv_animeList = findViewById(R.id.rv_animeList);
@@ -56,26 +85,117 @@ public class ListOfAnime extends AppCompatActivity {
         rv_animeList.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
 
         //crée & gere le recyclerView
-        JSONArray data = getJsonFromFile();
-        rv_animeList.setAdapter(new AnimeListAdapter(data));
+        data = getJsonFromFile();
+        mAdapter = new ListOfAnimeMV.AnimeListAdapter(data);
+        rv_animeList.setAdapter(mAdapter);
 
         DividerItemDecoration itemDecor = new DividerItemDecoration(rv_animeList.getContext(), VERTICAL);
         itemDecor.setOrientation(VERTICAL);
         rv_animeList.addItemDecoration(itemDecor);
 
-        btnAddAnime = findViewById(R.id.rv_button_add_anime);
+        rv_animeList.addOnItemTouchListener(
+                new RecyclerItemClickListener(this, rv_animeList ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
 
-        btnAddAnime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                    }
 
-                startActivity(addAnime);
+                    @Override public void onLongItemClick(final View view, final int position) {
 
-            }
-        });
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ListOfAnime.this);
+                        // set title of Alert
+                        alertDialogBuilder.setTitle(R.string.Delete_Anime);
 
+                        // set dialog message of Alert
+                        alertDialogBuilder
+                                .setMessage(R.string.Delete_Anime_msg)
+                                .setCancelable(false)
+                                .setPositiveButton(R.string.yes,new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+
+                                        try {
+
+                                            JSONObject obj = data.getJSONObject(position);
+
+                                            //obj.get("id")
+
+
+                                            // deletes this anime
+                                            String url = "https://apex.oracle.com/pls/apex/anime_keeper/ak/postDeleteAnime";
+
+                                            // Optional Parameters to pass as POST request
+                                            JSONObject js = new JSONObject();
+                                            try {
+                                                js.put("ID", obj.get("id"));
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+
+                                            // Make request for JSONObject
+                                            JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                                                    Request.Method.POST, url, js,
+                                                    new Response.Listener<JSONObject>() {
+                                                        @Override
+                                                        public void onResponse(JSONObject response) {
+                                                            Log.d("Info", response.toString());
+                                                        }
+                                                    }, new Response.ErrorListener() {
+                                                @Override
+                                                public void onErrorResponse(VolleyError error) {
+                                                    VolleyLog.d("Info", "Error: " + error.getMessage());
+                                                }
+                                            }) {
+
+
+                                                @Override
+                                                public Map<String, String> getHeaders() throws AuthFailureError {
+                                                    HashMap<String, String> headers = new HashMap<String, String>();
+                                                    headers.put("Content-Type", "application/json; charset=utf-8");
+                                                    return headers;
+                                                }
+
+                                            };
+
+                                            // Adding request to request queue
+                                            Volley.newRequestQueue(ListOfAnime.this).add(jsonObjReq);
+
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                        //startActivity(listAnime);
+                                        mAdapter.notifyDataSetChanged();
+
+                                    }
+                                })
+                                .setNegativeButton(R.string.no,new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+
+                                        // do nothing
+
+                                    }
+                                });
+
+                        // create alert dialog
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+
+                        // show it
+                        alertDialog.show();
+
+                    }
+                })
+        );
+    }
+
+
+    public static void MVVM_click(){
+
+        final Intent addAnime = new Intent(mContext, AddAnime.class);
+        mContext.startActivity(addAnime);
 
     }
+
+
 
     //get the notification that the json file has been received
     public static final String ANIME_LIST_UPDATE = "org.ledeme.animekeeper.ANIME_LIST_UPDATE";
@@ -84,72 +204,14 @@ public class ListOfAnime extends AppCompatActivity {
         public void onReceive(Context context, Intent intent){
             Log.d("test","ended");
 
-            AnimeListAdapter adapter = (AnimeListAdapter) rv_animeList.getAdapter();
+            ListOfAnimeMV.AnimeListAdapter adapter = (ListOfAnimeMV.AnimeListAdapter) rv_animeList.getAdapter();
             JSONArray data = getJsonFromFile();
             adapter.setNewAnimeList(data);
         }
 
     }
 
-    // ---------------------------------------------------------------------------------------------
 
-    //Adapter : c'est le tableau
-    private class AnimeListAdapter extends RecyclerView.Adapter<AnimeListAdapter.AnimeListHolder>{
-
-        //Holder : c'est la case
-        public class AnimeListHolder extends RecyclerView.ViewHolder {
-
-            public TextView name;
-            public TextView genres;
-
-            public AnimeListHolder(View view){
-                super(view);
-                name = view.findViewById(R.id.rv_anime_list_element_name);
-                genres = view.findViewById(R.id.rv_anime_list_element_genres);
-            }
-        }
-
-        private JSONArray animeList = new JSONArray();
-
-        public AnimeListAdapter(JSONArray AnimeList){
-            this.animeList = AnimeList;
-        }
-
-        public void setNewAnimeList(JSONArray newData){
-
-            this.animeList = newData;
-            notifyDataSetChanged();
-
-        }
-
-        @Override
-        public AnimeListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_anime_list_element, parent, false);
-
-            return new AnimeListHolder(itemView);
-        }
-
-        @Override
-        public void onBindViewHolder(AnimeListHolder holder, int position) {
-
-            try {
-                JSONObject data = animeList.getJSONObject(position);
-                holder.name.setText(getString(R.string.rv_anime_list_name) + data.getString("name"));
-                holder.genres.setText(getString(R.string.rv_anime_list_genres) + data.getString("genres"));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return animeList.length();
-        }
-    }
-
-    // ---------------------------------------------------------------------------------------------
 
     //Get the json from file
     public JSONArray getJsonFromFile(){
--- app/src/main/java/org/ledeme/animekeeper/ListOfAnimeMV.java
+++ app/src/main/java/org/ledeme/animekeeper/ListOfAnimeMV.java
@@ -1,6 +1,111 @@
 package org.ledeme.animekeeper;
 
+import android.databinding.ObservableField;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ListOfAnimeMV {
+
+    ListOfAnimeMV(){}
+
+    private ObservableField<String> name = new ObservableField<>("");
+    private ObservableField<String> genre = new ObservableField<>("");
+
+    public String getName(){
+        return name.get();
+    }
+    public void setName(ObservableField<String> name) {
+        this.name = name;
+        this.name.notifyChange();
+    }
+
+    public String getGenre(){
+        return genre.get();
+    }
+    public void setGenre(ObservableField<String> genre) {
+        this.genre = genre;
+        this.genre.notifyChange();
+    }
+
+    public void onClick(){
+
+        Log.i("truc", "clicked add anime");
+        ListOfAnime.MVVM_click();
+
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    //Adapter : c'est le tableau
+    public static class AnimeListAdapter extends RecyclerView.Adapter<AnimeListAdapter.AnimeListHolder>{
+
+        //Holder : c'est la case
+        public class AnimeListHolder extends RecyclerView.ViewHolder {
+
+            private TextView name;
+            private TextView genres;
+
+            private AnimeListHolder(View view){
+                super(view);
+                name = view.findViewById(R.id.rv_anime_list_element_name);
+                genres = view.findViewById(R.id.rv_anime_list_element_genres);
+            }
+        }
+
+        private JSONArray animeList;
+
+        AnimeListAdapter(JSONArray AnimeList){
+            animeList = AnimeList;
+        }
+
+        public void setNewAnimeList(JSONArray newData){
+
+            animeList = newData;
+            notifyDataSetChanged();
+
+        }
+
+        @Override
+        public AnimeListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_anime_list_element, parent, false);
+
+            return new AnimeListHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(AnimeListHolder holder, int position) {
+
+            try {
+                JSONObject data = animeList.getJSONObject(position);
+                holder.name.setText("Name : " + data.getString("name"));
+                holder.genres.setText("Genre : " + data.getString("genres"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return animeList.length();
+        }
+
+        public JSONObject getItem(int position) throws JSONException {
+            return animeList.getJSONObject(position);
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
 
 
 
